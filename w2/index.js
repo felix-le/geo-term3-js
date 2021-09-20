912c011e9a2dacbef64c62dcf2c3bd44dda4503e
Viet Anh Le <mc.vietanh@gmail.com>
2021-09-20 09:38:30 -0400

    updated

---
+++ w2/index.js
@@ -0,0 +1,45 @@
+const data = [
+    {
+      avatar: "https://robohash.org/debitisautemaspernatur.png?size=50x50&set=set1",
+      name: "Deloria Tern",
+      subject: "Finance",
+      body: "Aliquam augue quam, sollicitudin vitae, consectetuer eget, commodo placerat.",
+    },
+    {
+      avatar: "https://robohash.org/quasinmolestias.bmp?size=50x50&set=set1",
+      name: "Samaria Zorzetti",
+      subject: "Finance",
+      body: "Aenean auctor gravida sem.",
+    },
+    {
+      avatar: "https://robohash.org/dolorquaeratquia.jpg?size=50x50&set=set1",
+      name: "Bradford Hendrick",
+      subject: "Capital Goods",
+      body: "Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.",
+    },
+    {
+      avatar: "https://robohash.org/enimdolorumvitae.bmp?size=50x50&set=set1",
+      name: "Timmie Eacle",
+      subject: "Health Care",
+      body: "Integer tincidunt ante vel ipsum.",
+    },
+    {
+      avatar: "https://robohash.org/voluptasbeataeatque.bmp?size=50x50&set=set1",
+      name: "Evered Johantges",
+      subject: "n/a",
+      body: "Praesent lectus.",
+    },
+    {
+      avatar: "https://robohash.org/ipsamminimatempore.bmp?size=50x50&set=set1",
+      name: "Dorthea Gosz",
+      subject: "Finance",
+      body: "Morbi non quam nec dui luctus rutrum. Nulla tellus. In sagittis olestie lorem.",
+    },
+    {
+      avatar: "https://robohash.org/laudantiumaliasquisquam.bmp?size=50x50&set=set1",
+      name: "Ammamaria Kelby",
+      subject: "Technology",
+      body: "Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, turpis.",
+    },
+  ];
+  
